2f177f8349f2576c3c46f9078c03e3fddd74e6d6
Sokos6 <will.sokolowski@gmail.com>
2020-11-07 18:47:43 -0600

    returned data with groq

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -1,14 +1,10 @@
-// First, we must import the schema creator
-import createSchema from 'part:@sanity/base/schema-creator'
-
-// Then import schema types from any plugins that might expose them
-import schemaTypes from 'all:part:@sanity/base/schema-type'
-
-// We import object and document schemas
-import blockContent from './blockContent'
-import category from './category'
-import post from './post'
-import author from './author'
+import createSchema from 'part:@sanity/base/schema-creator';
+import schemaTypes from 'all:part:@sanity/base/schema-type';
+import blockContent from './blockContent';
+import category from './category';
+import post from './post';
+import author from './author';
+import comment from './comment';
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -22,8 +18,9 @@ export default createSchema({
     post,
     author,
     category,
+    comment,
     // When added to this list, object types can be used as
     // { type: 'typename' } in other document schemas
-    blockContent
-  ])
-})
+    blockContent,
+  ]),
+});
