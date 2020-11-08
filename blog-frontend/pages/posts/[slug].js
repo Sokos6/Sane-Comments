90471215f24119b42f82204c7fe776563660206c
Sokos6 <will.sokolowski@gmail.com>
2020-11-08 16:45:25 -0600

    working on form

--- blog-frontend/pages/posts/[slug].js
+++ blog-frontend/pages/posts/[slug].js
@@ -41,7 +41,8 @@ export default function Post({ post, comments, morePosts, preview }) {
               />
               <PostBody content={post.body} />
             </article>
-            <Comments comments={post?.comments} />
+            <Comments comments={post.comments} />
+            <Form _id={post._id} />
             <SectionSeparator />
             {morePosts.length > 0 && <MoreStories posts={morePosts} />}
           </>
